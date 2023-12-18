92cdebd1327ac90bd5a332601b5ec4d37754d948
alisajad001 <forwebonly0@gmail.com>
2023-12-18 09:43:53 +0430

    Style: Logo border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="w-full bg-gray-800 p-3">
-      <div className="border border-cyan-500 text-cyan-400 w-10 h-10 rounded-md flex justify-center items-center font-bold">
+      <div className="border border-cyan-400 text-cyan-400 w-10 h-10 rounded-md flex justify-center items-center font-bold">
         <h3>AS</h3>
       </div>
     </nav>
