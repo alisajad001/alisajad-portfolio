96ee094834d6a68f6ad96985d85aa3a51d269970
alisajad001 <forwebonly0@gmail.com>
2023-12-18 09:39:34 +0430

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full bg-gray-800 p-3">
+      <div className="border border-cyan-500 text-cyan-400 w-10 h-10 rounded-md flex justify-center items-center font-bold">
+        <h3>AS</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
