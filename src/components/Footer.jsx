9cf6cb1507134e8089f0fd873b2411d4342219f9
alisajad001 <forwebonly0@gmail.com>
2023-12-23 09:39:47 +0430

    Add: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full h-16 bg-gray-900 text-gray-200 flex justify-center items-center">
       <p>
-        Made with <span className="text-red-500">❤</span> by Ali Sajad Ahmmadi
+        Made with <span className="text-red-500">❤</span> by Ali Sajad Ahmadi
       </p>
     </div>
   );
