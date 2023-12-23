74d8444e0c0f985936e1472213199ec6f58f62d2
alisajad001 <forwebonly0@gmail.com>
2023-12-23 09:38:43 +0430

    Add: Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-16 bg-gray-900 text-gray-200 flex justify-center items-center">
+      <p>
+        Made with <span className="text-red-500">❤</span> by Ali Sajad Ahmmadi
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
