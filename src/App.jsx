9bd0cde743e9911e215248cb9aff1ab1a29e24ed
Ali Sajad <110373843+aliSajad12@users.noreply.github.com>
2024-01-15 14:34:16 +0430

    refactor: Coding from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import React from "react";
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      {/* <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer /> */}
-    </>
-  );
+  return <div>Hello World</div>;
 };
+
 export default App;
