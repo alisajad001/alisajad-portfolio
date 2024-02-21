5ba03221353fa24e614022e89af46ebf861f80c1
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-21 14:36:09 +0430

    refactor: Redesign and improve Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,50 +1,35 @@
+import { FaXTwitter, FaGithub, FaLinkedin } from 'react-icons/fa6';
+
 const Contact = () => {
   return (
-    <div className="w-full h-screen bg-gray-900" id="contact">
-      <div className="max-w-[1000px] h-full mx-auto p-5 flex flex-col">
-        <p className="font-bold text-4xl md:text-5xl text-gray-300 mb-4">
+    <section className="bg-gray-900">
+      <div className="max-w-[1000px] h-full mx-auto p-5">
+        <p className="font-bold text-4xl md:text-5xl text-gray-300">
           <span className="text-cyan-400 text-glow">04</span> Contact
         </p>
 
-        <p className="text-gray-300">Submit a form bellow</p>
-
-        <div className="mt-10 mx-auto w-full sm:w-[700px]">
-          <form
-            method="POST"
-            action="https://getform.io/f/e1948c3a-b171-4f3f-88a7-8389f9bd1d0b"
-            className="flex flex-col gap-3"
+        <div className="flex justify-center gap-6 mt-10">
+          <a
+            href="https://linkedin.com/in/alisajad001/"
+            className="text-white text-3xl"
           >
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              required
-              className="p-3 w-full sm:w-[700px] outline-none bg-gray-700 text-white rounded-md"
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-              className="p-3 w-full sm:w-[700px] outline-none bg-gray-700 text-white rounded-md"
-            />
-            <textarea
-              name="message"
-              rows="10"
-              className="p-3 w-full sm:w-[700px] outline-none bg-gray-700 text-white rounded-md"
-              placeholder="Message"
-              required
-            ></textarea>
-            <button
-              type="submit"
-              className="bg-cyan-400 p-3 rounded-md w-full sm:w-[700px]"
-            >
-              Submit
-            </button>
-          </form>
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/alisajad001"
+            className="text-white text-3xl"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://twitter.com/alisajad07"
+            className="text-white text-3xl"
+          >
+            <FaXTwitter />
+          </a>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
